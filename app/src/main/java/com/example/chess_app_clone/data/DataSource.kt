08cc19e4559bd2d8ffd2e29995bc1e636beff442
Ciocoiu Radu-Mihail <ciocoiuradu653@gmail.com>
2022-07-26 16:32:32 +0300

    commit

--- app/src/main/java/com/example/chess_app_clone/data/DataSource.kt
+++ app/src/main/java/com/example/chess_app_clone/data/DataSource.kt
@@ -5,12 +5,12 @@ import com.example.chess_app_clone.model.GeneralInfo
 
 class DataSource {
     fun loadPiecesInfo(): List<GeneralInfo> {
-        return listOf<GeneralInfo>(GeneralInfo(R.string.affirmation1, R.drawable.cal_negru),
-            GeneralInfo(R.string.affirmation2, R.drawable.nebun_negru),
-            GeneralInfo(R.string.affirmation3, R.drawable.pion_negru),
-            GeneralInfo(R.string.affirmation4, R.drawable.rege_negru),
-            GeneralInfo(R.string.affirmation5, R.drawable.regina_neagra),
-            GeneralInfo(R.string.affirmation6, R.drawable.turn_negru)
+        return listOf<GeneralInfo>(GeneralInfo(R.string.cal, R.drawable.cal_negru),
+            GeneralInfo(R.string.nebun, R.drawable.nebun_negru),
+            GeneralInfo(R.string.pion, R.drawable.pion_negru),
+            GeneralInfo(R.string.rege, R.drawable.rege_negru),
+            GeneralInfo(R.string.regina, R.drawable.regina_neagra),
+            GeneralInfo(R.string.turn, R.drawable.turn_negru)
         )
     }
 }
