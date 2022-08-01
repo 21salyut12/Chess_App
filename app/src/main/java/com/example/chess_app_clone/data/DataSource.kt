a68ef546f3913d1a42d9511be2c50014713d8240
alexm <alexmanole99@yahoo.com>
2022-08-01 19:46:30 +0300

    Schimbare poze

--- app/src/main/java/com/example/chess_app_clone/data/DataSource.kt
+++ app/src/main/java/com/example/chess_app_clone/data/DataSource.kt
@@ -5,24 +5,19 @@ import com.example.chess_app_clone.model.GeneralInfo
 
 class DataSource {
     fun loadPiecesInfo(): List<GeneralInfo> {
-        return listOf<GeneralInfo>(
-            GeneralInfo(R.string.cal, R.drawable.cal_negru),
-            GeneralInfo(R.string.cal, R.drawable.cal_alb),
+        return listOf(
+            GeneralInfo(R.string.cal, R.drawable.caiii),
 
-            GeneralInfo(R.string.nebun, R.drawable.nebun_negru),
-            GeneralInfo(R.string.nebun, R.drawable.nebun_alb),
+            GeneralInfo(R.string.nebun, R.drawable.nebunii),
 
-            GeneralInfo(R.string.pion, R.drawable.pion_negru),
-            GeneralInfo(R.string.pion, R.drawable.pion_alb),
+            GeneralInfo(R.string.pion, R.drawable.pionii),
 
-            GeneralInfo(R.string.affirmation4, R.drawable.rege_negru),
-            GeneralInfo(R.string.affirmation4, R.drawable.rege_alb),
+            GeneralInfo(R.string.rege, R.drawable.regiii),
 
-            GeneralInfo(R.string.affirmation5, R.drawable.regina_neagra),
-            GeneralInfo(R.string.affirmation5, R.drawable.regina_alba),
+            GeneralInfo(R.string.regina, R.drawable.damee),
 
-            GeneralInfo(R.string.affirmation6, R.drawable.turn_negru),
-            GeneralInfo(R.string.affirmation6, R.drawable.turn_alb)
+            GeneralInfo(R.string.turn, R.drawable.turee),
+
 
 
         )
