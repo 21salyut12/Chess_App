fbbb57cf9a523ea465c12a2d7ccea78389ab0c03
alexm <alexmanole99@yahoo.com>
2022-08-01 19:02:36 +0300

    Centrare

--- app/src/main/java/com/example/chess_app_clone/data/DataSource.kt
+++ app/src/main/java/com/example/chess_app_clone/data/DataSource.kt
@@ -5,12 +5,27 @@ import com.example.chess_app_clone.model.GeneralInfo
 
 class DataSource {
     fun loadPiecesInfo(): List<GeneralInfo> {
-        return listOf<GeneralInfo>(GeneralInfo(R.string.cal, R.drawable.cal_negru),
+        return listOf<GeneralInfo>(
+            GeneralInfo(R.string.cal, R.drawable.cal_negru),
+            GeneralInfo(R.string.cal, R.drawable.cal_alb),
+
             GeneralInfo(R.string.nebun, R.drawable.nebun_negru),
+            GeneralInfo(R.string.nebun, R.drawable.nebun_alb),
+
             GeneralInfo(R.string.pion, R.drawable.pion_negru),
-            GeneralInfo(R.string.rege, R.drawable.rege_negru),
-            GeneralInfo(R.string.regina, R.drawable.regina_neagra),
-            GeneralInfo(R.string.turn, R.drawable.turn_negru)
+            GeneralInfo(R.string.pion, R.drawable.pion_alb),
+
+            GeneralInfo(R.string.affirmation4, R.drawable.rege_negru),
+            GeneralInfo(R.string.affirmation4, R.drawable.rege_alb),
+
+            GeneralInfo(R.string.affirmation5, R.drawable.regina_neagra),
+            GeneralInfo(R.string.affirmation5, R.drawable.regina_alba),
+
+            GeneralInfo(R.string.affirmation6, R.drawable.turn_negru),
+            GeneralInfo(R.string.affirmation6, R.drawable.turn_alb)
+
+
         )
     }
 }
+
